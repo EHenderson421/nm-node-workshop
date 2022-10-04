a2a8c5aabaf2997bb616382b5f85d7819ac10d8f
Eric W. Henderson <ehenderson421@gmail.com>
2022-10-04 16:02:52 +0000

    Update index.js

--- posts/js-objects/index.js
+++ posts/js-objects/index.js
@@ -1,1 +1,27 @@
 console.log("Hello!");
+
+/*1. */
+const pokemon = {
+  name: "Pikachu",
+  images: [
+    {
+      alt: "Pikachu strutting",
+      url: "pokemon-images.com/pikachu-1.jpg",
+    },
+    {
+      alt: "Pikachu dancing",
+      url: "pokemon-images.com/pikachu-2.jpg",
+    },
+  ],
+};
+console.log(pokemon.images[0].alt);
+console.log(pokemon.images[1].url); /*This is a string data type*/
+
+/*2. */
+const message = {
+  content: "Hello, world!",
+  print: function () {
+    console.log(this.content);
+  },
+};
+message.print();
